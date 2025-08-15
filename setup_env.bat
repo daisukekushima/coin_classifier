c4cbbcad5139eb6069c94d3c76acd289490e88e7
daisukekushima <0xd4vk2z38z087v@ezweb.ne.jp>
2025-08-15 13:21:01 +0900

    fix setupfiles.bat

--- setup_env.bat
+++ setup_env.bat
@@ -2,15 +2,21 @@
 
 echo 仮想環境を作成
 SET ENV_NAME=.env_coin_classifier
-py -3.10 -m venv %ENV_NAME%
+if not exist %VENV_NAME%\Scripts\activate (
+    py -3.11 -m venv %ENV_NAME%
+    echo 仮想環境を作成しました。
+) else (
+    echo 仮想環境は既に存在します。
+)
+
 
 echo 仮想環境をアクティベート
 call %ENV_NAME%\Scripts\activate.bat
 
 
 echo ライブラリのインストール
-python -m pip install --upgrade pip
 pip install -r requirements.txt
 
 echo 仮想環境 %ENV_NAME% を作成しました。
 pause
+
